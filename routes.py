b4a86ea5947391b2cc772544f8f8f9efd5a8da95
saurabh <saurabh69912162@gmail.com>
2020-10-18 19:43:34 +0530

    :construction: Added Base HTML File

--- routes.py
+++ routes.py
@@ -1,3 +1,21 @@
+"""
+   Copyright 2020 Kartik Sharma, Saurabh Saxena
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+  ==============================================================================
+
+"""
+
+""" Flask App Routes """
+
 import os
 from flask import Flask
 from flask_login import LoginManager
@@ -7,5 +25,6 @@ app = Flask(__name__, template_folder='templates')
 
 app.secret_key = '7bc9b645bba64027bc9b645bba640269368993f2190db3369368993f2190db33'
 
-app.add_url_rule('/',view_func=main_views.HomePageView.as_view('home_page'))
+# Recarxiv Homepage Route
+app.add_url_rule('/',view_func=main_views.HomePageView.as_view('home_page_view'))
 
--- views/main_views.py
+++ views/main_views.py
@@ -1,8 +1,28 @@
+"""
+   Copyright 2020 Kartik Sharma, Saurabh Saxena
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+  ==============================================================================
+
+"""
+
+""" Flask App Main Views """
+
 import flask
 from flask.views import View, MethodView
 from flask import render_template, request, redirect, url_for, flash, jsonify, Flask, make_response, Response, send_file
 app = Flask(__name__,template_folder='././templates')
 
 class HomePageView(View):
+    '''Recarxiv Homepage View Handler'''
+    methods = ['GET']
     def dispatch_request(self):
         return render_template('homepage/homepage.html')
